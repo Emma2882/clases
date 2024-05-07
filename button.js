cf6555a8f65b8aa39049e69070c283325a71b731
Emma2882 <emmanuel2882@gmail.com>
2024-05-07 20:46:27 -0300

    primer commit

---
+++ button.js
@@ -0,0 +1,18 @@
+const alerta= document.querySelector(".formSobreMi");
+// document.querySelector("formSobreMi2")
+
+function funciona() {
+    
+    alerta.style.opacity="100";
+    // alerta2.style.opacity="0"
+    // alerta.style.opacity="0"
+   
+       
+    }
+    
+
+    
+    
+    
+
+
